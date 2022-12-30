185851ba30dd4cff6044f4f3cdfba7460066c5a5
Karan Singla <karan.singla@payufin.com>
2022-12-30 13:48:43 +0530

    adding dynamic and static lottie library

--- Package.swift
+++ Package.swift
@@ -4,7 +4,11 @@ import PackageDescription
 let package = Package(
   name: "Lottie",
   platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
-  products: [.library(name: "Lottie", targets: ["Lottie"])],
+  products: [
+    .library(name: "LottieDynamic", type: .dynamic , targets: ["Lottie"]),
+    .library(name: "LottieStatic", type: .static , targets: ["Lottie"]),
+    .library(name: "Lottie", targets: ["Lottie"]),
+            ],
   targets: [.target(name: "Lottie", path: "Sources")])
 
 #if swift(>=5.6)
